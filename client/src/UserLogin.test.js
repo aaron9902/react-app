e7dd1ee8698dc3e8b329ee1d9e17dc657a077fc1
aaron9902 <49785208+aaron9902@users.noreply.github.com>
2021-10-03 13:29:13 +1100

    Create New Unit test

---
+++ client/src/UserLogin.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Forums from '../src/components/forums';
+import { BrowserRouter, BrowserRouter as Router, Link } from "react-router-dom"; 
+
+describe('<Forums/ >', () => {
+  
+  it('matches snapshot', () => {
+    const wrapper = mount(<BrowserRouter><Forums /></BrowserRouter>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('it has input', () => {
+    const wrapper = mount(<BrowserRouter><Forums /></BrowserRouter>);
+    let input = wrapper.findWhere(
+      node => node.type() === 'input'
+    );
+    expect(input.exists()).toBe(true);
+  });
+
+});
