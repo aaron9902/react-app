cf614c7258ff34ae75551e35559956c96af06e84
MelvinHK <53848567+MelvinHK@users.noreply.github.com>
2021-10-19 20:53:52 +1100

    made upvotes css better. upvote counter is client side displayed for instant updating

--- client/src/components/Upvote.js
+++ client/src/components/Upvote.js
@@ -2,29 +2,30 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Upvote(props) {
-  const [threadData, setThreadData] = useState("");
+  const [votes, setVotes] = useState("");
 
   useEffect(() => {
     let isMounted = true;
     axios.get('/api/threads/' + props.id).then((res) => {
       if (isMounted) {
-        setThreadData(res.data);
+        setVotes(res.data.upvotes);
       }
     })
     return () => { isMounted = false };
-  })
+  }, [])
 
   const vote = (num) => {
     var data = {
       num: num
     };
     axios.patch('/api/threads/upvote/' + props.id, data);
+    setVotes(votes + num);
   }
 
   return (
     <div className="upvote">
       <a className="upvote-btn" onClick={() => { vote(1) }}><p>{'\u2B06'}</p></a>
-      <p>{threadData.upvotes}</p>
+      <p>{votes}</p>
       <a className="upvote-btn" onClick={() => { vote(-1) }}><p>{'\u2B07'}</p></a>
     </div>
   );
